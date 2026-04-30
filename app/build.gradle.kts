feac7f3274f1cd436c5847e8bbe1db362ea60920
Dennis Straßer <dennis.strasser@gmail.com>
2026-04-30 12:57:12 +0200

    Add extension manifest and bump version to 0.2
    Add manifest.json with extension description and release notes for
    the Karoo extension store. Bump versionCode to 2 and versionName
    to 0.2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.derstrassi.karoofirefly"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
     }
 
     signingConfigs {
