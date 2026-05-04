a247623b6094bcbec441e7b558d2cb5b1ce3d878
Dennis Straßer <dennis.strasser@gmail.com>
2026-05-04 11:57:19 +0200

    Bump version to 0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.derstrassi.karoofirefly"
         minSdk = 23
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.3"
+        versionCode = 5
+        versionName = "0.4"
     }
 
     signingConfigs {
